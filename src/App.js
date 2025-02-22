6c2474bf81c2c16f379ebfdbe2850b45e92cf843
Naowal Rahman <naowal.ar@gmail.com>
2025-02-22 13:54:41 -0500

    feat: Update App component and global styles
    - Implement main App component with Tetris game
    - Add global styles with styled-components
    - Configure dark theme
    - Clean up boilerplate code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Tetris from './components/Tetris';
+import { createGlobalStyle } from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background: #000;
+  }
+`;
+
+const App = () => (
+  <div className="App">
+    <GlobalStyles />
+    <Tetris />
+  </div>
+);
 
 export default App;
